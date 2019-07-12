f645dec84dd573efa2c209839c40d53b0b726006
Alena Nikulina <alenanikulina0@gmail.com>
2019-07-12 18:52:38 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -51,5 +51,13 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  sum = initial_round
+  until sum > 21
+    sum = hit?(sum)
+    display_card_total(sum)
+  end
+  end_game(sum)
 end
+
+runner 
